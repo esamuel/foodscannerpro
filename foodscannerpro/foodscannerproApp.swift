da984559bab1af1163d24bb3bbca7a15d449f854
Samuel <esamuel55@hotmail.com>
2025-03-12 13:25:24 +0200

    Initial Commit

---
+++ foodscannerpro/foodscannerproApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodscannerproApp.swift
+//  foodscannerpro
+//
+//  Created by Samuel Eskenasy on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct foodscannerproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodscannerproTests/foodscannerproTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodscannerproTests.swift
+//  foodscannerproTests
+//
+//  Created by Samuel Eskenasy on 3/12/25.
+//
+
+import Testing
+@testable import foodscannerpro
+
+struct foodscannerproTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
